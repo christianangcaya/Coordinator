10659bccb866d5adb7e6159753344d9677827351
Kenneth <bacunoabnerkenneth@gmail.com>
2024-12-11 10:06:21 +0800

    yes, sa login page ng coordinator

--- coordinator-ui/src/components/Login.jsx
+++ coordinator-ui/src/components/Login.jsx
@@ -25,43 +25,41 @@ const Login = () => {
   return (
     <div className="login-container">
       <div className="login-left">
-        <h1>Scholarship Filing Management System</h1>
-        <h2>LOCAL GOVERNMENT UNIT - DAET</h2>
         <div className="logo">
           <img src={logo} alt="Logo" />
         </div>
+        <h1>Scholarship Filing Management System</h1>
+        <h2>LOCAL GOVERNMENT UNIT - DAET</h2>
       </div>
       <div className="login-right">
-      <div className="square-container">
-        <h2>Sign In</h2>
-        <form onSubmit={handleLogin}>
-          <div className="form-group">
-            <label>Username:</label>
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label>Password:</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <a className="forgot-password">
-              Forgot your password?
-            </a>
-          </div>
-          {error && <p className="error">{error}</p>}
-          <button type="submit" className="login-button">
-            SIGN IN
-          </button>
-        </form>
-      </div>
+        <div className="square-container">
+          <h2>Sign In</h2>
+          <form onSubmit={handleLogin}>
+            <div className="form-group">
+              <label>Username:</label>
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Password:</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <a className="forgot-password">Forgot your password?</a>
+            </div>
+            {error && <p className="error">{error}</p>}
+            <button type="submit" className="login-button">
+              SIGN IN
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
