3413f87dd0ef4e81046f91f92d15f6b394ea45dc
Kenneth <bacunoabnerkenneth@gmail.com>
2024-12-16 21:36:59 +0800

    fix path passing. e.g. folder/2024/A-C.

--- coordinator-ui/src/App.jsx
+++ coordinator-ui/src/App.jsx
@@ -6,15 +6,14 @@ import StatusCard from "./components/StatusCard";
 import BatchFolders from "./components/BatchFolders";
 import SearchBar from "./components/SearchBar";
 import { useParams } from "react-router-dom";
-import BackButton from "./components/BackButton"
+import BackButton from "./components/BackButton";
 import ScholarsTable from "./components/ScholarsTable";
 import Login from "./components/Login";
 import "./App.css";
 import axios from "axios";
 
-
 const FolderDetails = () => {
-  const { folderName } = useParams(); 
+  const { folderName } = useParams();
   return (
     <div>
       <p>{folderName}</p>
@@ -23,7 +22,7 @@ const FolderDetails = () => {
 };
 
 const Home = () => {
-  const [folders, setFolders] = useState([]); 
+  const [folders, setFolders] = useState([]);
   const [error, setError] = useState("");
   const [batches, setBatches] = useState([]);
 
@@ -32,7 +31,7 @@ const Home = () => {
       try {
         const response = await axios.get("http://127.0.0.1:5000/api/folders");
         const fetchedFolders = response.data;
-        setFolders(fetchedFolders)
+        setFolders(fetchedFolders);
 
         setBatches(fetchedFolders);
       } catch (err) {
@@ -55,14 +54,26 @@ const Home = () => {
             <SearchBar />
           </div>
           <div className="status-section">
-          <StatusCard title="Appplicants" count="1,000" directory="scholar"/>
-          <StatusCard title="Pending Scholars" count="125" directory="scholar"/>
-            <StatusCard title="Active Scholars" count="1,025" directory="scholar"/>
-            <StatusCard title="Inactive Scholars" count="348" directory="scholar"/>
-            <StatusCard title="Alumni" count="9,547" directory="scholar"/>
+            <StatusCard title="Appplicants" count="1,000" directory="scholar" />
+            <StatusCard
+              title="Pending Scholars"
+              count="125"
+              directory="scholar"
+            />
+            <StatusCard
+              title="Active Scholars"
+              count="1,025"
+              directory="scholar"
+            />
+            <StatusCard
+              title="Inactive Scholars"
+              count="348"
+              directory="scholar"
+            />
+            <StatusCard title="Alumni" count="9,547" directory="scholar" />
           </div>
           <h3>Batch</h3>
-          <BatchFolders batches={batches} directory="folder"/>
+          <BatchFolders batches={batches} directory="folder" />
         </div>
       </div>
     </div>
@@ -78,7 +89,9 @@ const FolderPage = () => {
     if (folderName) {
       const fetchFolderData = async () => {
         try {
-          const response = await axios.get(`http://127.0.0.1:5000/api/folder/${folderName}`);
+          const response = await axios.get(
+            `http://127.0.0.1:5000/api/folder/${folderName}`
+          );
           setBatches(response.data);
         } catch (err) {
           console.error("Error fetching folder data:", err);
@@ -99,10 +112,16 @@ const FolderPage = () => {
           <div className="file-manager-header">
             <h2>Folder</h2>
             <SearchBar />
-            <BackButton/>
+            <BackButton />
           </div>
-          <h3><FolderDetails /></h3>
-          <BatchFolders batches={batches} directory="scholar" />
+          <h3>
+            <FolderDetails />
+          </h3>
+          <BatchFolders
+            batches={batches}
+            directory="scholar"
+            parentFolder={folderName}
+          />
         </div>
       </div>
     </div>
@@ -111,13 +130,12 @@ const FolderPage = () => {
 
 const ScholarshipsPage = () => {
   const [scholarshipData, setScholarshipData] = useState([]);
-
+  const { folderName, subfolderName } = useParams();
   const fetchScholarshipData = async () => {
     try {
-      const folderName = "2024"; 
-      const subfolderName = "A-C";
-
-      const response = await fetch(`http://127.0.0.1:5000/api/folder/${folderName}/${subfolderName}`);
+      const response = await fetch(
+        `http://127.0.0.1:5000/api/folder/${folderName}/${subfolderName}`
+      );
       if (!response.ok) {
         throw new Error(`Error: ${response.status}`);
       }
@@ -133,10 +151,12 @@ const ScholarshipsPage = () => {
   // Fetch data on component mount
   useEffect(() => {
     fetchScholarshipData();
-  }, []);
+  }, [folderName, subfolderName]);
 
   const scholars = scholarshipData.map((scholar) => ({
-    name: `${scholar.last_name}, ${scholar.first_name} ${scholar.middle_name} ${scholar.suffix || ""}`.trim(),
+    name: `${scholar.last_name}, ${scholar.first_name} ${scholar.middle_name} ${
+      scholar.suffix || ""
+    }`.trim(),
     year: scholar.applicant_id.slice(0, 4),
     address: `${scholar.street}, ${scholar.purok}, ${scholar.barangay}, ${scholar.municipality}`,
     school: scholar.school_name,
@@ -144,36 +164,39 @@ const ScholarshipsPage = () => {
     remarks: "Verified",
   }));
 
-
-
-  return(
+  return (
     <div className="app">
-    <Header />
-    <div className="main-content">
-      <div className="content">
-        <div className="file-manager-header">
-          <h2>Scholars</h2>
-          <SearchBar />
-          <BackButton />
+      <Header />
+      <div className="main-content">
+        <div className="content">
+          <div className="file-manager-header">
+            <h2>Scholars</h2>
+            <SearchBar />
+            <BackButton />
+          </div>
+          <h2>
+            <FolderDetails />
+          </h2>
+          <ScholarsTable scholars={scholars} />
         </div>
-        <h2><FolderDetails/></h2>
-      <ScholarsTable scholars={scholars} />
       </div>
     </div>
-  </div>
   );
 };
 
 const App = () => {
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />}/>
-          <Route path="/home" element={<Home />} />
-          <Route path="/folder/:folderName" element={<FolderPage />} />
-          <Route path="/scholar/:folderName" element={<ScholarshipsPage />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/folder/:folderName" element={<FolderPage />} />
+        <Route
+          path="/scholar/:folderName/:subfolderName"
+          element={<ScholarshipsPage />}
+        />
+      </Routes>
+    </Router>
   );
 };
 
--- coordinator-ui/src/components/BatchFolders.jsx
+++ coordinator-ui/src/components/BatchFolders.jsx
@@ -3,19 +3,28 @@ import "./BatchFolders.css";
 import folder from "../assets/folder.png";
 import { useNavigate } from "react-router-dom";
 
-const BatchFolders = ({ batches, directory }) => {
-  const navigate = useNavigate(); 
+const BatchFolders = ({ batches, directory, parentFolder = "" }) => {
+  const navigate = useNavigate();
 
   const handleFolderClick = (batch) => {
-    navigate(`/${directory}/${batch}`); 
-    // navigate("/folder")
+    if (parentFolder) {
+      // Navigate to subfolder (e.g., /scholar/2024/a-c)
+      navigate(`/${directory}/${parentFolder}/${batch}`);
+    } else {
+      // Navigate to main folder (e.g., /folder/2024)
+      navigate(`/${directory}/${batch}`);
+    }
   };
 
   return (
     <div className="batch-folders">
       {batches.map((batch, index) => (
         <div key={index} className="folder">
-          <img onClick={() => handleFolderClick(batch)} src={folder} alt="Folder" />
+          <img
+            onClick={() => handleFolderClick(batch)}
+            src={folder}
+            alt="Folder"
+          />
           <p onClick={() => handleFolderClick(batch)}>{batch}</p>
         </div>
       ))}
